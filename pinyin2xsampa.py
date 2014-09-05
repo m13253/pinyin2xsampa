67664b3720277fbf8bc7bfa09909a9f330e70646
Star Brilliant <m13253@hotmail.com>
2014-09-05 23:02:04 +0800

    Reformat as PEP8

--- pinyin2xsampa.py
+++ pinyin2xsampa.py
@@ -2,27 +2,29 @@
 
 import sys
 
-if sys.version_info<(3,):
+
+if sys.version_info < (3,):
     sys.stderr.write('This script requires Python 3 or higher version.\n')
     sys.exit(1)
 
-wholereplacetable=(
+
+wholereplacetable = (
     ('bo', 'buo'), ('po', 'puo'), ('mo', 'muo'), ('fo', 'fuo'), ('zhi', 'zh'),
     ('chi', 'ch'), ('shi', 'sh'), ('ri', 'r'), ('zi', 'z'), ('ci', 'c'),
     ('si', 's')
 )
-localreplacetable=(
+localreplacetable = (
     ('yu', 'v'), ('yi', 'i'), ('wu', 'u'), ('ju', 'jv'), ('qu', 'qv'),
     ('xu', 'xv'), ('y', 'i'), ('w', 'u'), ('\u00fc', 'v')
 )
-initialtable=(
+initialtable = (
     ('b', 'p'), ('p', 'p_h'), ('m', 'm'), ('f', 'f'), ('d', 't'), ('t', 't_h'),
     ('ng', 'N'), ('n', 'n'), ('l', 'l'), ('g', 'k'), ('k', 'k_h'), ('h', 'x'),
     ('j', 'ts\\'), ('q', 'ts\\_h'), ('x', 's\\'), ('zh', 'ts`'),
     ('ch', 'ts`_h'), ('sh', 's`'), ('r', 'z`'), ('z', 'ts'), ('c', 'ts_h'),
     ('s', 's')
 )
-finaltable=(
+finaltable = (
     ('\u00ea', 'E'), ('ai', 'aI'), ('ei', 'eI'), ('ao', 'AU'), ('ou', '@U'),
     ('ang', 'A N'), ('eng', '7 N'), ('an', 'a n'), ('ong', 'o N'),
     ('en', '@ n'), ('ie', 'iE'), ('iu', 'i@U'), ('ian', 'iE n'),
@@ -31,57 +33,57 @@ finaltable=(
     ('o', 'o'), ('e', 'MV'), ('i', 'i'), ('u', 'u'), ('v', 'y')
 )
 
+
 def main():
     while True:
         try:
-            line=input()
+            line = input()
         except EOFError:
             break
-        line=line.replace("'", ' ')
-        words=line.split()
-        pho=[]
+        line = line.replace("'", ' ')
+        words = line.split()
+        pho = []
         for word in words:
-            if word=='er':
+            if word == 'er':
                 pho.append('a r\\')
                 break
             if word.endswith('r'):
-                word=word[:-1]
-                endwithr='r\\'
+                word = word[:-1]
+                endwithr = 'r\\'
             else:
-                endwithr=''
+                endwithr = ''
             for i in wholereplacetable:
-                if word==i[0]:
-                    word=i[1]
+                if word == i[0]:
+                    word = i[1]
                     break
             for i in localreplacetable:
-                wordn=word.replace(i[0], i[1])
-                if wordn!=word:
-                    word=wordn
+                wordn = word.replace(i[0], i[1])
+                if wordn != word:
+                    word = wordn
                     break
-            initial=''
+            initial = ''
             for i in initialtable:
                 if word.startswith(i[0]):
-                    initial=i[1]
-                    word=word[len(i[0]):]
+                    initial = i[1]
+                    word = word[len(i[0]):]
                     break
-            final=''
+            final = ''
             while True:
-                foundfinal=False
+                foundfinal = False
                 for i in finaltable:
                     if word.startswith(i[0]):
-                        final+=i[1]
-                        word=word[len(i[0]):]
-                        foundfinal=True
+                        final += i[1]
+                        word = word[len(i[0]):]
+                        foundfinal = True
                 if not foundfinal:
                     break
             if not final:
                 if initial in ('ts`', 'ts`_h', 's`', 'z`'):
-                    final='M'
+                    final = 'M'
                 elif initial in ('ts', 'ts_h', 's'):
-                    final='1'
+                    final = '1'
             pho.append(' '.join((initial, final, endwithr)).strip())
-        print(' '.join(('['+i+']' for i in pho)).strip())
+        print(' '.join(('[' + i + ']' for i in pho)).strip())
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
